249a7ca93e610f017d231ddb1d7fe87712c5139f
Rahul Shah <rahul@mynextdeveloper.com>
2024-08-01 22:30:07 +0530

    Add task endpoints

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { TaskService } from './task.service';
 
+const USER_ID = 1;
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  async fetchTasks() {
+    return this.taskService.fetchTasks(USER_ID);
+  }
+
+  @Post()
+  async postTask(@Body() body: Prisma.TaskCreateInput) {
+    return this.taskService.createTask(USER_ID, body);
+  }
+
+  @Patch(':id')
+  async patchTask(@Param('id') taskId, @Body() body: Prisma.TaskUpdateInput) {
+    return this.taskService.updateTask(USER_ID, taskId, body);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') taskId) {
+    return this.taskService.deleteTask(USER_ID, taskId);
+  }
+}
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  createTask(userId: number, payload: Prisma.TaskCreateInput) {
+    return this.prisma.task.create({
+      data: { ...payload, user: { connect: { id: userId } } },
+    });
+  }
+
+  fetchTasks(userId: number) {
+    return this.prisma.task.findMany({ where: { userId: userId } });
+  }
+
+  updateTask(userId: number, taskId: number, payload: Prisma.TaskUpdateInput) {
+    return this.prisma.task.update({
+      where: { id: taskId, userId },
+      data: payload,
+    });
+  }
+
+  deleteTask(userId: number, taskId: number) {
+    return this.prisma.task.delete({ where: { id: taskId, userId } });
+  }
+}
